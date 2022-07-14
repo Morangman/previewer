1cfdaa3ea11170c25d9a4ea33214ae8a76475b13
Morangman <sinepolsky.dmitry@gmail.com>
2022-07-14 21:19:04 +0300

    Changes

--- header.php
+++ header.php
@@ -13,7 +13,7 @@ $isAdmin = isset($_SESSION['is_admin']);
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="/">Контакти</a>
+            <a class="nav-link active" aria-current="page" href="/">(beta version)</a>
         </li>
       </ul>
     </div>
